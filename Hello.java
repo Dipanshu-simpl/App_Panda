d4882f3031d5be02eeb4d998d2aa5ad31b8f2002
Dipanshu Magoo <dipanshu.magoo96@gmail.com>
2020-07-19 13:04:10 +0530

    Doing some changes in dev branch

--- Hello.java
+++ Hello.java
@@ -3,9 +3,13 @@ import java.util.*;
 import java.lang.*;
 class Main{
 	public static void main(String args[])
-	{
-		System.out.println("Hello World");
+	{     
+		// Adding an Loop here changes doing in dev branch
+	 	for(int i=0;i<5;i++)
+		{
 
+		System.out.println("Hello World");
+		}
 	}
 }
 
